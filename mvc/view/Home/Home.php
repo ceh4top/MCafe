b20a9644724fb04e8f4f1d6ab6a45e7b5f302272
Alina Popova <popowa.alinochka@gmail.com>
2018-07-16 03:19:46 +0300

    Contacts

--- mvc/view/Home/Home.php
+++ mvc/view/Home/Home.php
@@ -54,7 +54,7 @@
     <section id="Banquet">
         <div class="container">
             <div class="row">
-                <div class="BlockImages col-12 col-md-5">
+                <div class="BlockImages col-12 col-md-6">
                     <div class="row">
                         <div class="Image col-6"><div class="Img img1"></div></div>
                         <div class="Image col-6"><div class="Img img2"></div></div>
@@ -62,10 +62,40 @@
                         <div class="Image col-6"><div class="Img img4"></div></div>
                     </div>
                 </div>
-                <div class="BlockText col-12 col-md-7">
+                <div class="BlockText col-12 col-md-6">
                     <div class="Title">Описание</div>
+                    <div class="IconLine"><div class="Icon"></div></div>
                     <div class="Text">
                         <p>Размышляете над вопросом: где отметить #свадьбу, #корпоратив, #юбилей?<br/>Удачно выбрав место проведения праздника, вы обеспечите успех мероприятия уже наполовину. Ведь свадьба, корпоратив, юбилей, который организован по всем правилам, с учетом различных нюансов, позволит в полной мере наслаждаться красивым, со вкусом сделанным праздником. Стильный интерьер, доступные, адекватные цены, отличное обслуживание- все это делает персонал #Mcafevrn одним их лучших мест для проведения банкетов. Свадебный банкетный зал «Mcafe» подойдет как для больших мероприятий, так и для тех, кто празднует в кругу близких. Команда профессионалов окажет помощь в организации праздника без хлопот в независимости от бюджета и формата, что сделает его безупречным.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="Contacts" class="Title Block">
+        <div class="Image ParallaxScrolling">
+            <div class="Name"><span>Контакты</span></div>
+        </div>
+    </section>
+    <section id="Contacts">
+        <div class="container">
+            <div class="row">
+                <div class="Map col-12 col-md-6">
+                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A54cc151ef2c3eb4770e7b833834f2b4ff5f6fe248f8a3faa6eec9ab532832f26&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=false"></script>
+                </div>
+                <div class="Contact col-12 col-md-6">
+                    <div class="Title"> Связь с нами </div>
+                    <div class="IconLine"><div class="Icon"></div></div>
+                    <div class="Text">
+                        <p><i class="fas fa-mobile-alt"></i> +7(951)854-10-94 </p>
+                        <p><i class="fas fa-map-marker-alt"></i> Воронеж, ул. Московский Проспект 4 </p>
+                        <p><i class="fas fa-envelope-open"></i> mcafevrn@yandex.ru </p>
+                        <br>
+                        <p class="TextSocial"> Мы в социальных сетях: </p>
+                        <div class="IconLine">
+                            <a class="IconSocial" href="https://www.instagram.com/m_cafevrn/"><i class="fab fa-instagram"></i></a>
+                            <a class="IconSocial" href="https://vk.com/m_cafevrn"><i class="fab fa-vk"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
